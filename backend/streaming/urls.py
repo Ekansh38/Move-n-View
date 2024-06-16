018ed3e56733477842a613449b7ea1e85174ca7e
Ekansh Goenka <goenka.ekansh@gmail.com>
2024-06-16 13:32:22 +0800

    coded a basic server and client

--- backend/streaming/urls.py
+++ backend/streaming/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("stream/", views.video_stream, name="stream"),
+    path("video_feed/", views.video_feed, name="video_feed"),
+    path("upload_frame", views.upload_frame, name="upload_frame"),
 ]
--- backend/streaming/views.py
+++ backend/streaming/views.py
@@ -1,10 +1,33 @@
+import cv2
+from django.core.files.storage import default_storage
+from django.http import JsonResponse, StreamingHttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+latest_frame_path = "latest_frame.jpg"
+
+
+def upload_frame(request):
+    if request.method == "POST":
+        frame = request.FILES["frame"]
+        with default_storage.open(latest_frame_path, "wb+") as destination:
+            for chunk in frame.chunks():
+                destination.write(chunk)
+        return JsonResponse({"status": "frame received"})
+    return JsonResponse({"status": "bad request"}, status=400)
+
+
+def gen_frames():
+    while True:
+        with default_storage.open(latest_frame_path, "rb") as frame_file:
+            frame = frame_file.read()
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
 def video_feed(request):
-    pass
+    return StreamingHttpResponse(
+        gen_frames(), content_type="multipart/x-mixed-replace; boundary=frame"
+    )
 
 
 def video_stream(request):
